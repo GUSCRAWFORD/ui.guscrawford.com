3c19a2b0dea18f7cb3f391ea327094e69f0bbe70
Gus Crawford <gus.crawford@envisageny.com>
2018-09-01 19:48:58 -0400

    Add a hardcoded blog entry, test material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +9,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to guscrawford.com</h1>
         </header>
+        <h2>React Refresh</h2>
+        <h3>September 1, 2018</h3>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Just because I never even got started keeping a real blog of my work and challenges; I've decided to refresh my site using React (just because it's simple, popular, and I've probably oversaturated myself with Angular by this point)
+        </p>
+        <p>
+          A lot of activity over the last couple months publishing packages and working to develop some test frameworks for different contexts which should generate some points for me to talk about in the upcoming future!
         </p>
       </div>
     );
